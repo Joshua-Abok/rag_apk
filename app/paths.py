5180283c25b0ca0f9d11845d9bcbdb993e2135e2
Michela Agostini <michelaagostini73@gmail.com>
2025-06-24 13:23:02 +0200

    commit upload path

--- app/paths.py
+++ app/paths.py
@@ -5,10 +5,7 @@ OUTPUTS_DIR = os.path.join(ROOT_DIR, "outputs")
 
 
 DATA_DIR = os.path.join(ROOT_DIR, "data")
-#PUBLICATION_FPATH = os.path.join(DATA_DIR, "publication.md")
-#PUBLICATION_FPATH = os.path.join(DATA_DIR, "yzN0OCQT7hUSd.md")
 
-#VECTOR_DB_DIR = os.path.join(OUTPUTS_DIR, "vector_db")
 
 LOADER_DIR = os.path.join(ROOT_DIR, "loaders")
 
